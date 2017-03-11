f3d07bb571e722e28a084d7cb53b8afc0eeea229
Bariz <bsudhanshu@gmail.com>
2017-03-10 16:08:30 -0800

    Add logic for all 4 card networks

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,25 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  if (cardNumber.length === 14 && cardNumber.substring(0, 2) === "38" 
+  	|| cardNumber.length === 14 && cardNumber.substring(0, 2) === "39") {
+  	return "Diner's Club";
+  } else if (cardNumber.length === 15 && cardNumber.substring(0, 2) === "34" 
+  	|| cardNumber.length === 15 && cardNumber.substring(0, 2) === "37") {
+    return 'American Express';
+  } else if (cardNumber.length === 13 && cardNumber.charAt(0) === "4" 
+  	|| cardNumber.length === 16 && cardNumber.charAt(0) === "4" 
+  	|| cardNumber.length === 19 && cardNumber.charAt(0) === "4") {
+      return 'Visa';
+  } else if (cardNumber.length === 16 && cardNumber.substring(0, 2) === "51"
+  	|| cardNumber.length === 16 && cardNumber.substring(0, 2) === "52"
+  	|| cardNumber.length === 16 && cardNumber.substring(0, 2) === "53"
+  	|| cardNumber.length === 16 && cardNumber.substring(0, 2) === "54"
+  	|| cardNumber.length === 16 && cardNumber.substring(0, 2) === "55") {
+      return 'MasterCard';
+  } else {
+  	return undefined;
+  }
 };
 
 
